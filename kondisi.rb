6a4ade11733c0aa51b0f5a3a9b64a0a91cff21e7
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-20 19:27:59 +0700

    Update kondisi.rb to adjust passing score to 60 and enhance feedback messages; add password validation examples

--- kondisi.rb
+++ kondisi.rb
@@ -8,8 +8,20 @@ nilai3 = gets.to_i
 
 nilai = (nilai1 + nilai2 + nilai3) / 3
 
-if nilai >= 50
-  print "anda lulus dengan score #{nilai}"
+if nilai >= 60
+  print "anda lulus dengan score rata - rata #{nilai} Selamat yah"
 else
-  print "anda tidak lulus dengan score #{nilai}"
+  print "anda tidak lulus dengan score rata - rata #{nilai} jangan menyerah"
 end
+
+#Dasark Kondisi IF ELSE blok
+print "\n"
+password = "12345"
+if password == "12345"
+  print "Password benar"
+else
+  print "Password salah"
+end
+
+#penulisan if else dengan satu baris
+puts "password benar" if password == "12345"
