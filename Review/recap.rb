11e0d3a367c284937b080af1f70b9d89ba14977a
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-21 19:19:22 +0700

    Add Ruby examples for variable usage, conditionals, loops, and arrays

---
+++ Review/recap.rb
@@ -0,0 +1,20 @@
+#variabel
+nama = "Rizal"
+
+#if-Else
+if nama == "Rizal"
+  puts "Hello Rizal"
+else
+  puts "Hello Stranger"
+end
+
+#loops
+3.times do
+  puts "Hello Rizal"
+end
+
+#array
+teman = ["Rizal", "Rizky", "Rizwan"]
+teman.each do |teman|
+  puts "Hello #{teman}"
+end  
