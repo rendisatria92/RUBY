0beda45813c54c60cf665b6ab3746e06adf0b378
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-22 10:54:48 +0700

    Add quiz program with questions for user interaction

---
+++ Submission/quiz.rb
@@ -0,0 +1,28 @@
+print "Selamat Datan di Quiz Ruby"
+print "Jawablah pertanyaan berikut ini"
+
+puts "1.Siapa nama pemeran utama serial Yu-gi-oh?"
+puts "A. Yugi"
+puts "B. Joey"
+puts "C. Tristan"
+puts "D. Bakura"
+print "Jawaban kamu : "
+Jawaban1 = gets.chop
+
+puts "2.Siapa nama pemeran utama serial Naruto?"
+puts "A. Naruto"
+puts "B. Sasuke"
+puts "C. Sakura"
+print "Jawaban kamu : "
+Jawaban2 = gets.chomp
+
+puts "3.Siapa nama pemeran utama serial One Piece?"
+puts "A. Luffy"
+puts "B. Zoro"
+puts "C. Nami"
+puts "D. Sanji"
+print "Jawaban kamu : "
+Jawaban3 = gets.chomp
+
+
+
