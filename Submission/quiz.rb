27b5586b6b064f1017b5925a6b44a7dea344cbea
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-22 20:40:09 +0700

    Fix greeting message and implement scoring system in quiz.rb

--- Submission/quiz.rb
+++ Submission/quiz.rb
@@ -1,4 +1,4 @@
-print "Selamat Datan di Quiz Ruby"
+print "Selamat Datang di Quiz Ruby"
 print "Jawablah pertanyaan berikut ini"
 
 puts "1.Siapa nama pemeran utama serial Yu-gi-oh?"
@@ -7,22 +7,16 @@ puts "B. Joey"
 puts "C. Tristan"
 puts "D. Bakura"
 print "Jawaban kamu : "
-Jawaban1 = gets.chop
 
-puts "2.Siapa nama pemeran utama serial Naruto?"
-puts "A. Naruto"
-puts "B. Sasuke"
-puts "C. Sakura"
-print "Jawaban kamu : "
-Jawaban2 = gets.chomp
-
-puts "3.Siapa nama pemeran utama serial One Piece?"
-puts "A. Luffy"
-puts "B. Zoro"
-puts "C. Nami"
-puts "D. Sanji"
-print "Jawaban kamu : "
-Jawaban3 = gets.chomp
-
-
-
+jawaban_1 = gets.chop
+s_core = self.score(jawaban_1)
+def self.score (jawaban)
+  if jawaban == "A"
+    s_score = 10
+  else
+    s_score = 0
+  end
+  return s_core
+end
+s_core = self.score(Jawaban_1)
+puts "Hasil Quiz kamu adalah #{s_core}"
