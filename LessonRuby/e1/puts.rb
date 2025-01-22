3e116452d9cfcc5e780f81ec892eae1df3dcf2ca
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-23 04:55:38 +0700

    Update Ruby learning materials with additional examples and explanations

--- LessonRuby/e1/puts.rb
+++ LessonRuby/e1/puts.rb
@@ -1,19 +1,30 @@
-#Pembelajaran Ruby Awal
+#Pembelajaran Ruby Awal 23 Januari 2025
 #Menegnal Puts dan Print
 
 puts "Hello, World!" 
-#Puts akan menampilkan string di bawahnya
+#Puts akan menampilkan string di baris bawahnya
 
 print "Hello, World!" 
 #print akan menampilkan string di baris yang sama
 
-#Contoh Lain
+#Contoh nya !
 puts "Hello, World!"
 puts "Nama Saya adalah Rendi Satria."
 puts "Saya sedang belajar pemograma Ruby."
 puts "Saya bercita - cita manejadi seorang Full Stack."
 
 #Puts akan menampilkan string di bawahnya
-print 'Saya lulusan S1 Tekbik informatika "Uiversitas Esa Unggul, "'
+print 'Saya lulusan S1 Teknik informatika "Uiversitas Esa Unggul, "'
 print 'Sejak kuliah saya ingin membangun suatu Perusahaan yang bernama "R-Game Studio Animation".'
+
+#Penggunaan Singel quote ''  dan  Double quote "" dibolehkan, perbedaannya adalah kenika menggunakan tanda Singel quote jika dalam satu baris code membutuhkan pemanggulan variabel maka, nilai nya akan bersifat string.
+
+#Contoh !
+print 'Sejak belajar Linux saya senang senang dengan #{Ruby} '
+puts  'Belajar Linux Sangat Menyenangkan, cobalah mulai belajar dan belajar #{Ruby} ikut penyenangkan'
+
+#Penggunaan tanda # di dalam string, artinya adalah menggantikan nilai
+#Penggunaan #{ } untuk memanggil variabel, tapi perlu diingat harus mengunakan "" Double quote
+
+
 
