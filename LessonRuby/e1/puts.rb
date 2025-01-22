b9de56b13d49d1854cdd6c8e36c1e1c1c4929a16
rendisatria92 <mrendisatria1992@gmail.com>
2025-01-23 04:42:21 +0700

    Add initial Ruby learning materials with examples of puts and print

---
+++ LessonRuby/e1/puts.rb
@@ -0,0 +1,19 @@
+#Pembelajaran Ruby Awal
+#Menegnal Puts dan Print
+
+puts "Hello, World!" 
+#Puts akan menampilkan string di bawahnya
+
+print "Hello, World!" 
+#print akan menampilkan string di baris yang sama
+
+#Contoh Lain
+puts "Hello, World!"
+puts "Nama Saya adalah Rendi Satria."
+puts "Saya sedang belajar pemograma Ruby."
+puts "Saya bercita - cita manejadi seorang Full Stack."
+
+#Puts akan menampilkan string di bawahnya
+print 'Saya lulusan S1 Tekbik informatika "Uiversitas Esa Unggul, "'
+print 'Sejak kuliah saya ingin membangun suatu Perusahaan yang bernama "R-Game Studio Animation".'
+
